c51b47cec4d7858a985a04e78819acb79b8d03e2
mhammeDev <mhammedvipp@gmail.com>
2024-04-09 12:19:13 +0200

    Creation of the interactive map (which represents a house in the specifications) + drag and drop logic

--- FRONT/easing/src/components/HouseMap.vue
+++ FRONT/easing/src/components/HouseMap.vue
@@ -1,11 +1,212 @@
-<script setup>
-
-</script>
-
 <template>
+        <v-btn @click="toggleFloor" color="primary">Basculer entre les étages, étage actuelle : {{currentFloor}}</v-btn>
+          <v-stage :config="{ width: stageWidth, height: stageHeight }">
+           <v-layer>
+           <v-group  v-for="piece in tabPiece" :key="piece.id">
+                <v-shape
+                         :config="getShapeConfig(piece)"/>
+                <v-text
+                    :config="getTextConfig(piece)" />
 
+          <v-regular-polygon
+                :config="getPolygonConfig()"
+                ></v-regular-polygon>
+
+              </v-group>
+            </v-layer>
+
+
+            <v-layer>
+              <v-group :draggable="true"
+                       @dragstart="dragStart"
+                      @dragend="dragEnd">
+
+                <v-circle :config="headConfig()"></v-circle>
+
+              </v-group>
+            </v-layer>
+
+          </v-stage>
 </template>
 
-<style scoped>
+<script>
+import { defineComponent, onMounted, ref, computed } from 'vue';
+import { VStage, VLayer, VShape,VText, VGroup, VRegularPolygon,VCircle  } from 'vue-konva';
+import { pieces } from "@/data/pieces";
 
-</style>
+export default defineComponent({
+  components: {
+    VStage,
+    VLayer,
+    VShape,
+    VText,
+    VGroup,
+    VRegularPolygon,
+    VCircle,
+  },
+  setup() {
+    const stageWidth = ref(window.innerWidth);
+    const stageHeight = ref(window.innerHeight);
+    const currentFloor = ref('first');
+    // let coeff = ref(1)
+
+    window.addEventListener('resize', () => {
+      // coeff.value = 100 * window.innerWidth / stageWidth
+
+
+    //  stageWidth.value = stageWidth.value * (window.innerWidth/100);
+      // stageHeight.value = stageHeight.value * (window.innerHeight/100);
+    });
+
+
+
+    onMounted(() => {
+      stageWidth.value = window.innerWidth;
+      stageHeight.value = window.innerHeight;
+    });
+
+    const dragStart = (event) =>{
+      console.log(event)
+      document.body.style.cursor = "grab";
+    }
+
+    const pointInPolygon = (point, polygon) => {
+      let isInside = false;
+      const x = point.x, y = point.y;
+
+      for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
+        const xi = polygon[i].x, yi = polygon[i].y;
+        const xj = polygon[j].x, yj = polygon[j].y;
+
+        console.log(xi, ' ', xj)
+        console.log(x, ' ', y)
+
+
+        const intersect = ((yi > y) !== (yj > y)) &&
+            (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+        if (intersect) isInside = !isInside;
+      }
+
+      return isInside;
+    };
+
+    const dragEnd = (event) => {
+      document.body.style.cursor = 'default';
+      const stage = event.target.getStage();
+      const pointerPosition = stage.getPointerPosition();
+
+      const droppedOnPiece = tabPiece.value.find(piece => {
+        return pointInPolygon(pointerPosition, piece.position.points);
+      });
+
+      if (droppedOnPiece) {
+        console.log('Dropped on piece:', droppedOnPiece.nom);
+      } else {
+        console.log('Dropped outside of the pieces');
+      }
+    };
+
+    const toggleFloor = () => {
+      currentFloor.value = currentFloor.value === 'first' ? 'second' : 'first';
+    };
+
+    const tabPiece = computed(() => {
+      if(currentFloor.value === 'first') return pieces.filter(piece => piece.etage === 0);
+      else return pieces.filter(piece => piece.etage !== 0);
+    });
+
+
+    const getShapeConfig = (piece) => {
+      return {
+        sceneFunc: (context, shape) => {
+          context.beginPath();
+          piece.position.points.forEach((point, index) => {
+            if (index === 0) {
+              context.moveTo(point.x * 0.900, point.y* 0.900);
+            } else {
+              context.lineTo(point.x * 0.900, point.y * 0.900);
+            }
+          });
+          context.closePath();
+          context.fillStrokeShape(shape);
+        },
+        fill: 'gray',
+        stroke: 'black',
+        strokeWidth: 2
+      };
+    };
+
+    const getPolygonConfig = () => {
+      return {
+        x: 300,
+        y: 300,
+        sides: 6,
+        radius: 15,
+        fill: 'purple',
+        stroke: 'black',
+        strokeWidth: 2
+      };
+    };
+
+    const getCenterPoint = (points) => {
+      let sumX = 0;
+      let sumY = 0;
+      points.forEach(point => {
+        sumX += point.x ;
+        sumY += point.y;
+      });
+      return { x: sumX / points.length  , y: sumY / points.length };
+    };
+
+    const getTextConfig = (piece) => {
+      const center = getCenterPoint(piece.position.points);
+      return {
+        text: piece.nom,
+        fontSize: 18,
+        fontFamily: 'Mono',
+        x: center.x * 0.900,
+        y: center.y * 0.900 ,
+        align: 'center',
+        verticalAlign: 'middle',
+        fill: 'black'
+      };
+    };
+
+    const headConfig = () => {
+      return{
+        x: 1000,
+        y: 800,
+        radius: 8,
+        fill: '#ED7F10'
+      }
+
+    }
+
+
+/*    const handleMouseOver = (event) => {
+      //event.target.fill('red');
+      //event.target.getLayer().draw();
+    };
+
+    const handleMouseOut = (event) => {
+      //event.target.fill('gray');
+      //event.target.getLayer().draw();
+    };*/
+
+
+    return {
+      getShapeConfig,
+      stageWidth,
+      stageHeight,
+      toggleFloor,
+      currentFloor,
+      tabPiece,
+      getTextConfig,
+      getPolygonConfig,
+      headConfig,
+      dragStart,
+      dragEnd,
+    };
+  }
+});
+</script>
---
+++ FRONT/easing/src/data/pieces.js
@@ -0,0 +1,279 @@
+export const pieces = [
+  {
+    id:1,
+    "nom": "Chambre 1",
+    "etage":1,
+    "superficie": "9.71 m²",
+    "position": {
+      "points": [
+        {"x": 0, "y": 0},
+        {"x": 350, "y": 0},
+        {"x": 350, "y": 350},
+        {"x": 0, "y": 350}
+      ]
+    },
+    "capteurs": [
+      { "type": "Capteur de présence", "quantite": 1, "etat" : false }
+    ],
+    "actionneurs": [
+    ]
+  },
+  {
+    id:2,
+    "nom": "Salle parentale",
+    "superficie": "7.27 m²",
+    "position": {
+      "points": [
+        {"x": 0, "y": 350},
+        {"x": 350, "y": 350},
+        {"x": 350, "y": 600},
+        {"x": 0, "y": 600}
+      ]
+    },"etage":1,
+    "capteurs": [
+      { "type": "Capteur de présence", "quantite": 1, "etat" :false },
+      {"type": "Détecteur de fuite d'eau", "quantite" : 1, "etat" : false}
+    ],
+    "actionneurs": [
+    //  { "type": "Ampoule connectée", "quantite": 1 },
+      //{ "type": "Assistant vocal", "quantite": 1 }
+    ]
+  },
+  {
+    id:3,
+    "nom": "Chambre 2",
+    "etage":1,
+    "superficie": "14.09 m²",
+    "position":
+        {"points": [
+            {"x": 0, "y": 600},
+            {"x": 500, "y": 600},
+            {"x": 500, "y": 900},
+            {"x": 0, "y": 900},
+          ]},
+    "capteurs": [
+      { "type": "Capteur de présence", "quantite": 1, "etat" : false }
+    ],
+    "actionneurs": [
+      { "type": "Prise connectée", "quantite": 1 },
+      { "type": "Store motorisé", "quantite": 1 }
+    ]
+  },  {
+    id:4,
+    "nom": "",
+    "superficie": "10.0 m²",
+    "position": {
+      "points": [
+        {"x": 350, "y": 600},
+        {"x": 500, "y": 600},
+        {"x": 500, "y": 300},
+        {"x": 600, "y": 300},
+        {"x": 600, "y": 200},
+        {"x": 1050, "y": 200},
+        {"x": 1050, "y": 0},
+        {"x": 500, "y": 0},
+        {"x": 500, "y": 150},
+        {"x": 350, "y": 150},
+        {"x": 350, "y": 600},
+      ]
+    },
+    "etage": 1,
+    "capteurs": [
+      { "type": "Capteur de présence", "quantite": 1, "etat" : false }
+    ],
+    "actionneurs": [
+      {"type": "Lumière connectée", "quantite": 1},
+      {"type": "Lumière connectée prise intelligente", "quantite": 1}
+    ]
+  },
+  {
+    id:5,
+    "nom": "Salle de bain",
+    "superficie": "5.49 m²",
+    "etage":1,
+    "position": {
+      "points": [
+        {"x": 1050, "y": 200},
+        {"x": 1350, "y": 200},
+        {"x": 1350, "y": 0},
+        {"x": 1050, "y": 0},
+        {"x": 1050, "y": 200},
+
+      ]
+    },    "capteurs": [
+      { "type": "Capteur de présence", "quantite": 1, "etat" : false },
+      //{ "type": "Détecteur de fuite d'eau", "quantite": 1 }
+    ],
+    "actionneurs": [
+     // { "type": "Ampoule connectée", "quantite": 1 }
+    ]
+  },
+  {
+    id:6,
+    "nom": "WC",
+    "superficie": "1.9 m²",
+    "position": {
+      "points": [
+        {"x": 350, "y": 0},
+        {"x": 500, "y": 0},
+        {"x": 500, "y": 150},
+        {"x": 350, "y": 150}
+
+      ]
+    },"etage":1,
+    "capteurs": [
+      { "type": "Capteur de présence", "quantite": 1, "etat" : false },
+    ],
+    "actionneurs": [
+    //  { "type": "Ampoule connectée", "quantite": 1 },
+     // { "type": "Assistant vocal", "quantite": 1 }
+    ]
+  },  {
+    id:7,
+    "nom": "Pièce 1",
+    "etage":1,
+    "superficie": "12.93 m²",
+    "position": {
+      "points": [
+        {"x": 500, "y": 600},
+        {"x": 950, "y": 600},
+        {"x": 950, "y": 200},
+        {"x": 600, "y": 200},
+        {"x": 600, "y": 300},
+        {"x": 500, "y": 300},
+        {"x": 500, "y": 600},
+      ]
+    },
+    "capteurs": [
+      { "type": "Capteur de présence", "quantite": 1, "etat" : false },
+    ],
+    "actionneurs": [
+    ]
+  },{
+    id:8,
+    "nom": "Pièce 2",
+    "etage":1,
+    "superficie": "12.93 m²",
+    "position": {
+      "points": [
+        {"x": 950, "y": 600},
+        {"x": 1350, "y": 600},
+        {"x": 1350, "y": 200},
+        {"x": 950, "y": 200},
+      ]
+    },
+    "capteurs": [
+      { "type": "Capteur de présence", "quantite": 1, "etat" : false },
+    ],
+    "actionneurs": [
+    ]
+  },
+  {
+    id:9,
+    "nom": "Salle à manger",
+    "superficie": "40.0 m²",
+    "etage":0,
+    "position": {
+      "points": [
+        {"x": 0, "y": 0},
+        {"x": 500, "y": 0},
+        {"x": 500, "y": 850},
+        {"x": 0, "y": 850},
+
+
+      ]
+    },    "capteurs": [
+      { "type": "Capteur de présence", "quantite": 1, "etat" : false },
+    ],
+    "actionneurs": [
+      { "type": "Ampoule connectée", "quantite": 1 },
+      { "type": "Prise connectée pour TV", "quantite": 1 },
+      { "type": "Store motorisé", "quantite": 3 }
+    ]
+  },
+  {
+    id:10,
+    "nom": "Pièce 3",
+    "superficie": "4.21 m²",
+    "etage":0,
+    "position": {
+      "points": [
+        {"x": 500, "y": 0},
+        {"x": 950, "y": 0},
+        {"x": 950, "y": 200},
+        {"x": 500, "y": 200},
+
+
+      ]
+    },    "capteurs": [
+      { "type": "Capteur de présence", "quantite": 1, "etat" : false },
+    ],
+    "actionneurs": []
+  },
+  {
+    id:11,
+    "nom": "Hall d'entrée",
+    "superficie": "14.29 m²",
+    "etage":0,
+    "position": {
+      "points": [
+        {"x": 500, "y": 200},
+        {"x": 950, "y": 200},
+        {"x": 950, "y": 550},
+        {"x": 500, "y": 550},
+      ]
+    },
+    "capteurs": [
+      { "type": "Capteur de présence", "quantite": 1, "etat" : false },
+    ],
+    "actionneurs": [
+      { "type": "Prise connectée pour TV", "quantite": 1 },
+      { "type": "Thermostat connecté", "quantite": 1 }
+    ]
+  },
+  {
+    id:12,
+    "nom": "Cuisine",
+    "superficie": "18.0 m²",
+    "etage":0,
+    "position": {
+      "points": [
+        {"x": 950, "y": 550},
+        {"x": 1350, "y": 550},
+        {"x": 1350, "y": 0},
+        {"x": 950, "y": 0},
+      ]
+    }, "capteurs": [
+      { "type": "Capteur de présence", "quantite": 1, "etat" : false },
+    ],
+    "actionneurs": [
+      { "type": "Ampoule connectée", "quantite": 1 },
+    ]
+  },
+
+]
+
+export const type_capteur = [
+  { "_id": "sensor-presence", "type": "Capteur de présence", "description": "Détecte la présence dans la pièce" },
+  { "_id": "sensor-water-leak", "type": "Détecteur de fuite d'eau", "description": "Détecte les fuites d'eau" },
+  { "_id": "sensor-light", "type": "Lumière connectée", "description": "Contrôle l'éclairage de la pièce" },
+  { "_id": "sensor-smart-plug", "type": "Prise connectée", "description": "Permet de contrôler les appareils électriques" },
+  { "_id": "sensor-motorized-blind", "type": "Store motorisé", "description": "Contrôle l'ouverture et la fermeture des stores" }
+]
+
+export const  test = [
+  {
+    "nom": "Salon",
+    "superficie": "16.94 m²",
+    "position": {"x": 10, "y": 100, "largeur": 480, "hauteur": 240},
+    "etage":0,
+    "capteurs": [
+      { "type": "Capteur de présence", "quantite": 1 }
+    ],
+    "actionneurs": [
+      { "type": "Ampoule connectée", "quantite": 1 },
+      { "type": "Prise connectée pour TV", "quantite": 1 }
+    ]
+  }
+]
+

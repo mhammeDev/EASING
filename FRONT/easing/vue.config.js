837e74b06e4d53cd8884cc0f39c4732f7ee43aaf
mhammeDev <mhammedvipp@gmail.com>
2024-04-16 09:41:15 +0200

    Initializing backend

--- FRONT/easing/vue.config.js
+++ FRONT/easing/vue.config.js
@@ -1,8 +1,27 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path');
+
+
+
 module.exports = defineConfig({
   transpileDependencies: true,
 
-  pluginOptions: {
+//     publicPath: process.env.NODE_ENV === 'production' ? '/easing/' : '/',
+
+    outputDir: 'dist',
+    assetsDir: 'assets',
+
+   /* devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                changeOrigin: true,
+                pathRewrite: { '^/api': '' },
+            }
+        }
+    },*/
+
+    pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
 		}
